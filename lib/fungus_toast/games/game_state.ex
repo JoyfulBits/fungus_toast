3e9d6c9b4b63f1e8e136b1e6361fd9f97a9173da
Dave Shah <david.a.shah@gmail.com>
2019-01-24 21:05:32 -0500

    add at/3

--- lib/fungus_toast/games/game_state.ex
+++ lib/fungus_toast/games/game_state.ex
@@ -28,5 +28,17 @@ defmodule FungusToast.Games.Game.State do
     def new(rows, cols), do: 1..cols |> Enum.reduce([], fn _, acc -> acc ++ [row(rows)] end)
 
     defp row(len), do: 1..len |> Enum.reduce([], fn _, acc -> acc ++ [%Cell{}] end)
+
+    @doc ~S"""
+    Creates a new grid with empty cells
+
+     ## Examples
+
+
+     iex> grid = [[%FungusToast.Games.Game.State.Cell{cell_type: 1, player_id: nil}],[%FungusToast.Games.Game.State.Cell{cell_type: nil, player_id: nil}]]
+     iex> Grid.at(grid, 0,0)
+     %FungusToast.Games.Game.State.Cell{cell_type: 1, player_id: nil}
+    """
+    def at(grid, row, col), do: grid |> Enum.at(row) |> Enum.at(col)
   end
 end
