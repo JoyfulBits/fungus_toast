1d8f261017665d94485941f5b98cdf0c9fee6f9e
Dave Shah <david.a.shah@gmail.com>
2019-01-24 20:54:53 -0500

    Introduce Grid

--- lib/fungus_toast/games/game_state.ex
+++ lib/fungus_toast/games/game_state.ex
@@ -4,6 +4,29 @@ defmodule FungusToast.Games.Game.State do
 
   defmodule Cell do
     @derive Jason.Encoder
-    defstruct [:index, :player_id, :cell_type]
+    defstruct [:player_id, :cell_type]
+  end
+
+  defmodule Grid do
+    @doc ~S"""
+    Creates a new grid with empty cells
+
+     ## Examples
+
+     iex> Grid.new(2,2)
+     [
+       [
+         %FungusToast.Games.Game.State.Cell{cell_type: nil, player_id: nil},
+         %FungusToast.Games.Game.State.Cell{cell_type: nil, player_id: nil}
+       ],
+       [
+         %FungusToast.Games.Game.State.Cell{cell_type: nil, player_id: nil},
+         %FungusToast.Games.Game.State.Cell{cell_type: nil, player_id: nil}
+       ]
+     ]
+    """
+    def new(rows, cols), do: 1..cols |> Enum.reduce([], fn _, acc -> acc ++ [row(rows)] end)
+
+    defp row(len), do: 1..len |> Enum.reduce([], fn _, acc -> acc ++ [%Cell{}] end)
   end
 end
---
+++ test/fungus_toast/games/grid_test.exs
@@ -0,0 +1,6 @@
+defmodule FungusToast.Games.GridTest do
+  use ExUnit.Case, async: true
+  alias FungusToast.Games.Game.State.Grid
+  doctest Grid
+
+end
