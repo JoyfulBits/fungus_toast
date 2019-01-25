1250f932bd3ca48a4ea9bd79afb7f994cc1d34b8
Dave Shah <david.a.shah@gmail.com>
2019-01-24 21:28:42 -0500

    add replace/4

--- lib/fungus_toast/games/grid.ex
+++ lib/fungus_toast/games/grid.ex
@@ -4,36 +4,57 @@ defmodule FungusToast.Games.Grid do
     defstruct [:player_id, :cell_type]
   end
 
-    @doc ~S"""
-    Creates a new grid with empty cells
+  @doc ~S"""
+  Creates a new grid with empty cells
 
-     ## Examples
+   ## Examples
 
-     iex> Grid.new(2,2)
+   iex> Grid.new(2,2)
+   [
      [
-       [
-         %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil},
-         %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}
-       ],
-       [
-         %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil},
-         %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}
-       ]
+       %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil},
+       %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}
+     ],
+     [
+       %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil},
+       %FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}
      ]
-    """
-    def new(rows, cols), do: 1..cols |> Enum.reduce([], fn _, acc -> acc ++ [row(rows)] end)
+   ]
+  """
+  def new(rows, cols), do: 1..cols |> Enum.reduce([], fn _, acc -> acc ++ [row(rows)] end)
 
-    defp row(len), do: 1..len |> Enum.reduce([], fn _, acc -> acc ++ [%Cell{}] end)
+  defp row(len), do: 1..len |> Enum.reduce([], fn _, acc -> acc ++ [%Cell{}] end)
 
-    @doc ~S"""
-    Returns the cell at row, col
+  @doc ~S"""
+  Returns the cell at row, col
 
-     ## Examples
+   ## Examples
 
 
-     iex> grid = [[%FungusToast.Games.Grid.Cell{cell_type: 1, player_id: nil}],[%FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}]]
-     iex> Grid.at(grid, 0,0)
-     %FungusToast.Games.Grid.Cell{cell_type: 1, player_id: nil}
-    """
-    def at(grid, row, col), do: grid |> Enum.at(row) |> Enum.at(col)
+   iex> grid = [[%FungusToast.Games.Grid.Cell{cell_type: 1, player_id: nil}],[%FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}]]
+   iex> Grid.at(grid, 0,0)
+   %FungusToast.Games.Grid.Cell{cell_type: 1, player_id: nil}
+  """
+  def at(grid, row, col), do: grid |> Enum.at(row) |> Enum.at(col)
+
+  @doc ~S"""
+  Replaces the cell at row, col with val
+
+   ## Examples
+
+
+   iex> grid = [[%FungusToast.Games.Grid.Cell{cell_type: 1, player_id: nil}],[%FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}]]
+   iex> Grid.replace(grid, 0,0, %FungusToast.Games.Grid.Cell{cell_type: 10, player_id: 100})
+   [
+     [%FungusToast.Games.Grid.Cell{cell_type: 10, player_id: 100}],
+     [%FungusToast.Games.Grid.Cell{cell_type: nil, player_id: nil}]
+   ]
+  """
+  def replace(grid, row, col, val) do
+    new_row =
+      Enum.at(grid, row)
+      |> List.replace_at(col, val)
+
+    List.replace_at(grid, row, new_row)
+  end
 end
