9d080dd1f381b565cc4b07d8f943212939adc3a9
Chris Rees <crees@covermymeds.com>
2019-02-04 21:25:23 -0500

    Add game stats to players

--- lib/fungus_toast/players/player.ex
+++ lib/fungus_toast/players/player.ex
@@ -4,12 +4,50 @@ defmodule FungusToast.Players.Player do
 
   @attrs [
     :human,
+    :mutation_points,
+    :top_left_growth_chance,
+    :top_growth_chance,
+    :top_right_growth_chance,
+    :right_growth_chance,
+    :bottom_right_growth_chance,
+    :bottom_growth_chance,
+    :bottom_left_growth_chance,
+    :left_growth_chance,
+    :dead_cells,
+    :live_cells,
+    :regenerated_cells,
+    :apoptosis_chance,
+    :starved_cell_death_chance,
+    :mutation_chance,
+    :regeneration_chance,
+    :mycotoxin_fungicide_chance
   ]
 
   @derive {Jason.Encoder, only: [:id] ++ @attrs}
 
   schema "players" do
-    field :human, :boolean, default: false
+    field :human, :boolean, default: false, null: false
+
+    field :mutation_points, :integer, default: 0, null: false
+
+    field :top_left_growth_chance, :decimal, default: 0.0, null: false
+    field :top_growth_chance, :decimal, default: 0.0, null: false
+    field :top_right_growth_chance, :decimal, default: 0.0, null: false
+    field :right_growth_chance, :decimal, default: 0.0, null: false
+    field :bottom_right_growth_chance, :decimal, default: 0.0, null: false
+    field :bottom_growth_chance, :decimal, default: 0.0, null: false
+    field :bottom_left_growth_chance, :decimal, default: 0.0, null: false
+    field :left_growth_chance, :decimal, default: 0.0, null: false
+
+    field :dead_cells, :integer, default: 0, null: false
+    field :live_cells, :integer, default: 0, null: false
+    field :regenerated_cells, :integer, default: 0, null: false
+
+    field :apoptosis_chance, :decimal, default: 0.0, null: false
+    field :starved_cell_death_chance, :decimal, default: 0.0, null: false
+    field :mutation_chance, :decimal, default: 0.0, null: false
+    field :regeneration_chance, :decimal, default: 0.0, null: false
+    field :mycotoxin_fungicide_chance, :decimal, default: 0.0, null: false
 
     belongs_to :user, FungusToast.Accounts.User, foreign_key: :user_id
 
@@ -22,7 +60,7 @@ defmodule FungusToast.Players.Player do
   @doc false
   def changeset(player, attrs) do
     player
-    |> cast(attrs, [:human])
-    |> validate_required([:human])
+    |> cast(attrs, @attrs)
+    |> validate_required(@attrs)
   end
 end
---
+++ priv/repo/migrations/20190204175431_add_game_stats_to_players.exs
@@ -0,0 +1,25 @@
+defmodule FungusToast.Repo.Migrations.AddGameStatsToPlayers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:players) do
+      add :mutation_points, :integer, default: 0, null: false
+      add :top_left_growth_chance, :decimal, default: 0.0, null: false
+      add :top_growth_chance, :decimal, default: 0.0, null: false
+      add :top_right_growth_chance, :decimal, default: 0.0, null: false
+      add :right_growth_chance, :decimal, default: 0.0, null: false
+      add :bottom_right_growth_chance, :decimal, default: 0.0, null: false
+      add :bottom_growth_chance, :decimal, default: 0.0, null: false
+      add :bottom_left_growth_chance, :decimal, default: 0.0, null: false
+      add :left_growth_chance, :decimal, default: 0.0, null: false
+      add :dead_cells, :integer, default: 0, null: false
+      add :live_cells, :integer, default: 0, null: false
+      add :regenerated_cells, :integer, default: 0, null: false
+      add :apoptosis_chance, :decimal, default: 0.0, null: false
+      add :starved_cell_death_chance, :decimal, default: 0.0, null: false
+      add :mutation_chance, :decimal, default: 0.0, null: false
+      add :regeneration_chance, :decimal, default: 0.0, null: false
+      add :mycotoxin_fungicide_chance, :decimal, default: 0.0, null: false
+    end
+  end
+end
