e32b07c66862842bacfd00fe55a5845ed3f7da24
Chris Rees <crees@covermymeds.com>
2019-02-06 21:00:10 -0500

    Only seed when explicitly told to do so

--- mix.exs
+++ mix.exs
@@ -57,8 +57,9 @@ defmodule FungusToast.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
