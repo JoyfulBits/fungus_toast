099e6970f1070d31a58f0fe162fcdb5d6196ac4c
Chris Rees <crees@covermymeds.com>
2019-02-06 22:39:50 -0500

    Add documentation to the PlayerSkills helper

--- lib/fungus_toast/games/helpers/player_skills.ex
+++ lib/fungus_toast/games/helpers/player_skills.ex
@@ -9,6 +9,18 @@ defmodule FungusToast.PlayerSkills do
   alias FungusToast.Games
   alias FungusToast.Games.{Player, PlayerSkill, Skill}
 
+  @doc """
+  Returns the skill information for a given player.
+
+  ## Examples
+
+      iex> get_player_skill(player, 1)
+      %PlayerSkill{}
+
+      iex> get_player_skill(1, 1)
+      %Player{}
+
+  """
   def get_player_skill(%Player{} = player, skill_id) do
     get_player_skill(player.id, skill_id)
   end
@@ -16,6 +28,18 @@ defmodule FungusToast.PlayerSkills do
     from(ps in PlayerSkill, where: ps.player_id == ^player_id and ps.skill_id == ^skill_id) |> Repo.one()
   end
 
+  @doc """
+  Returns information about all skills for a given player.
+
+  ## Examples
+
+      iex> get_player_skills(player)
+      [%PlayerSkill{}, ...]
+
+      iex> get_player_skills(1)
+      [%Player{}, ...]
+
+  """
   def get_player_skills(%Player{} = player) do
     get_player_skills(player.id)
   end
@@ -23,6 +47,24 @@ defmodule FungusToast.PlayerSkills do
     from(ps in PlayerSkill, where: ps.player_id == ^player_id) |> Repo.all()
   end
 
+  @doc """
+  Creates a new PlayerSkill mapping a player to a skill with level, etc.
+
+  ## Examples
+
+      iex> create_player_skill(player, skill, %{field: new_value})
+      {:ok, %PlayerSkill{}}
+
+      iex> create_player_skill(1, 1, %{field: new_value})
+      {:ok, %PlayerSkill{}}
+
+      iex> create_player_skill(player, skill, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+      iex> create_player_skill(1, 1, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def create_player_skill(player, skill, attrs \\ %{})
   def create_player_skill(%Player{} = player, %Skill{} = skill, attrs) when is_map(attrs) do
     create_player_skill(player.id, skill.id, attrs)
@@ -33,6 +75,21 @@ defmodule FungusToast.PlayerSkills do
     |> Repo.insert()
   end
 
+  @doc """
+  Updates skill information for a given player, if they have the appropriate number of mutation points.
+
+  ## Examples
+
+      iex> update_player_skills(player, [%{"id": 1, "spent_points": 1}, ...])
+      {:ok, [%PlayerSkill{}, ...]}
+
+      iex> update_player_skills(1, [%{"id": 1, "spent_points": 1}, ...])
+      {:ok, [%PlayerSkill{}, ...]}
+
+      iex> update_player_skills(player, [%{"id": 1, "spent_points": -1}, ...])
+      {:error, :bad_request}
+
+  """
   def update_player_skills(%Player{} = player, skill_upgrades) when is_list(skill_upgrades) do
     valid_request = is_valid_request?(player, skill_upgrades)
     update_player_skills(%Player{} = player, skill_upgrades, [], valid_request)
@@ -70,11 +127,15 @@ defmodule FungusToast.PlayerSkills do
     {:error, :bad_request}
   end
 
-  def is_valid_request?(%Player{} = player, skill_upgrades) do
+  defp is_valid_request?(%Player{} = player, skill_upgrades) do
     points_spent = sum_skill_upgrades(skill_upgrades, 0)
     player.mutation_points >= points_spent
   end
 
+  @doc """
+  Returns how many mutation points are required for all of the requested skill upgrades
+
+  """
   def sum_skill_upgrades([head | tail], accum) do
     sum_skill_upgrades(tail, accum + Map.get(head, "points_spent"))
   end
@@ -82,6 +143,18 @@ defmodule FungusToast.PlayerSkills do
     accum
   end
 
+  @doc """
+  Updates a player skill.
+
+  ## Examples
+
+      iex> update_player_skill(player_skill, %{field: new_value})
+      {:ok, %PlayerSkill{}}
+
+      iex> update_player_skill(player_skill, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def update_player_skill(%PlayerSkill{} = player_skill, attrs) do
     player_skill
     |> PlayerSkill.changeset(attrs)
