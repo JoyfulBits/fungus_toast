75bb59e8e79d26ee09d946a9df653646f5cf457e
jakejgordon <jakejgordon@gmail.com>
2019-02-19 22:19:21 -0500

    Provide additional error messages

--- lib/fungus_toast/games/grid.ex
+++ lib/fungus_toast/games/grid.ex
@@ -6,30 +6,32 @@ defmodule FungusToast.Games.Grid do
   def create_starting_grid(number_of_rows_and_columns, player_ids) do
     number_of_players = length(player_ids)
 
-    initial_grid_cells_map =
-      Enum.into(
-        1..number_of_players,
-        %{},
-        &{&1, get_start_cell_index(number_of_rows_and_columns, number_of_players, &1)}
-      )
-
-    starting_grid =
-      Enum.reduce(initial_grid_cells_map, %{}, fn {player_id, position}, map ->
-        Map.put(map, position, %GridCell{
-          index: position,
-          player_id: player_id,
-          live: true,
-          empty: false
-        })
-      end)
-
-    if Map.size(starting_grid) < number_of_players do
+    if(number_of_rows_and_columns < 10) do
       {:error,
-       "#{number_of_players} players is too many players for grid size of #{
-         number_of_rows_and_columns
-       }x#{number_of_rows_and_columns}"}
+      "A grid size of #{number_of_rows_and_columns}x#{number_of_rows_and_columns} is too small. The minimum grid size is 10x10."}
     else
-      starting_grid
+      number_of_empty_cells_after_placing_start_cells = number_of_rows_and_columns * number_of_rows_and_columns - number_of_players
+      if(number_of_empty_cells_after_placing_start_cells < 100) do
+        {:error,
+        "There needs to be at least 100 cells left over after placing starting cells, but there was only #{number_of_empty_cells_after_placing_start_cells}."}
+      else
+        initial_grid_cells_map =
+        Enum.into(
+          1..number_of_players,
+          %{},
+          &{&1, get_start_cell_index(number_of_rows_and_columns, number_of_players, &1)}
+        )
+
+        starting_grid =
+          Enum.reduce(initial_grid_cells_map, %{}, fn {player_id, position}, map ->
+            Map.put(map, position, %GridCell{
+              index: position,
+              player_id: player_id,
+              live: true,
+              empty: false
+            })
+          end)
+      end
     end
   end
 
--- test/fungus_toast/games/grid_test.exs
+++ test/fungus_toast/games/grid_test.exs
@@ -4,36 +4,39 @@ defmodule FungusToast.Games.GridTest do
 
   describe "create_starting_grid/3" do
     test "that each player gets a new cell" do
-      new_grid = Grid.create_starting_grid(20, [10, 20, 30])
+      player_ids = [10, 20, 30]
+      new_grid = Grid.create_starting_grid(20, player_ids)
 
-      assert Enum.count(new_grid) == 3
-    end
-
-    defp find_grid_cell_for_player(grid, player_id) do
-      tuple = Enum.find(grid, fn {_, v} -> v.player_id == player_id end)
-      elem(tuple, 1)
+      assert Enum.count(new_grid) == length(player_ids)
     end
 
     test "that a large enough grid adequeqtly places the number of players" do
       player_ids = [1,2,3,4,5]
-      grid = Grid.create_starting_grid(10, player_ids)
+      grid = Grid.create_starting_grid(20, player_ids)
 
       assert Map.size(grid) == length(player_ids)
     end
 
-    test "that an error is returned if too many players are added to the grid" do
+    test "that an error is returned if the grid is too small" do
       player_ids = [1,2,3,4,5]
 
       error =
         Grid.create_starting_grid(2, player_ids)
 
-      assert {:error, "5 players is too many players for grid size of 2x2"} = error
+      assert {:error, "A grid size of 2x2 is too small. The minimum grid size is 10x10."} = error
+    end
+
+    test "that an error is returned if there are too many players to allow for at least 100 empty spaces" do
+      error =
+        Grid.create_starting_grid(10, [1])
+
+      assert {:error, "There needs to be at least 100 cells left over after placing starting cells, but there was only 99."} = error
     end
 
     test "that a starting cell is not empty, is alive, and is assigned to the player" do
       player1Id = 1
 
-      new_grid = Grid.create_starting_grid(10, [player1Id])
+      new_grid = Grid.create_starting_grid(20, [player1Id])
 
       player_cell = find_grid_cell_for_player(new_grid, player1Id)
 
@@ -44,6 +47,11 @@ defmodule FungusToast.Games.GridTest do
       start_index = hd(Map.keys(new_grid))
       assert player_cell.index == start_index
       refute player_cell.previous_player_id
+    end
+
+    defp find_grid_cell_for_player(grid, player_id) do
+      tuple = Enum.find(grid, fn {_, v} -> v.player_id == player_id end)
+      elem(tuple, 1)
     end
   end
 end
