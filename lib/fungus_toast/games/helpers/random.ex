2ca34acebdedb0ef261ca4bbbab61110f0325276
jakejgordon <jakejgordon@gmail.com>
2019-03-01 22:19:09 -0500

    added documentation

--- lib/fungus_toast/games/helpers/random.ex
+++ lib/fungus_toast/games/helpers/random.ex
@@ -1,4 +1,7 @@
 defmodule FungusToast.Random do
+    @doc """
+    Takes a percentage chance (float value) and returns true if that percentage chance randomly hits
+    """
     @spec random_chance_hit(float()) :: boolean()
     def random_chance_hit(percent_chance) do
         Enum.random(0..9999) < percent_chance * 100
