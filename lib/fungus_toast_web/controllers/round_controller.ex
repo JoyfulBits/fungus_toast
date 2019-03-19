7853dd4685981dc1cbb35ca4ee44ff8a82fe4327
jakejgordon <jakejgordon@gmail.com>
2019-03-19 12:52:09 -0400

    removed unused action

--- lib/fungus_toast_web/controllers/round_controller.ex
+++ lib/fungus_toast_web/controllers/round_controller.ex
@@ -6,17 +6,6 @@ defmodule FungusToastWeb.RoundController do
 
   action_fallback FungusToastWeb.FallbackController
 
-  # TODO: move the preload into Games
-  def create(conn, %{"game_id" => game_id, "round" => round_params}) do
-    with {:ok, %Round{} = round} <- Games.create_round(game_id, round_params) do
-      round = round |> FungusToast.Repo.preload(:game)
-
-      conn
-      |> put_status(:created)
-      |> render("show.json", round: round)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     round = Games.get_round!(id)
     render(conn, "show.json", round: round)
