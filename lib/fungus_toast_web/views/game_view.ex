312b47fa60d0589fb87a7eec419b70305dcc5c9a
Dave Shah <david.a.shah@gmail.com>
2019-03-20 06:03:49 -0400

    WIP - game to json transformation

--- lib/fungus_toast_web/views/game_view.ex
+++ lib/fungus_toast_web/views/game_view.ex
@@ -10,5 +10,19 @@ defmodule FungusToastWeb.GameView do
     render_one(game, GameView, "game.json")
   end
 
-  def render("game.json", %{game: game}), do: map_from(game)
+  def render("game.json", %{game: game}) do 
+    transform_game_fields(game)
+    |> transform_player_fields()
+  end
+
+  defp transform_game_fields(game) do
+    {value, map} = map_from(game) |> Map.pop(:grid_size)
+    Map.put_new(map, :number_of_rows, value)
+    |> Map.put_new(:number_of_columns, value)
+    |> Map.put_new(:number_of_cells, trunc(:math.pow(value, 2)))
+  end
+
+  defp transform_player_fields(%{players: _players} = game) do
+    game # TODO: tansform
+  end
 end
--- test/fungus_toast_web/views/game_view_test.exs
+++ test/fungus_toast_web/views/game_view_test.exs
@@ -8,14 +8,20 @@ defmodule FungusToastWeb.GameViewTest do
   describe "game.json" do
     @stub_game %Game{ 
         id: "fake",
+        number_of_human_players: 1,
+        number_of_ai_players: 1,
         players: [%Player{}, %Player{}],
         rounds: [%Round{}, %Round{}]}
 
     test "the transformation of data model to json" do
         assert GameView.render("game.json", %{game: @stub_game}) == 
           %{
-              # TODO: provide the structure we ACTUALLY want
-
+              number_of_human_players: 1,
+              number_of_ai_players: 1,
+              number_of_rows: 50,
+              number_of_columns: 50,
+              status: "Not Started",
+              number_of_cells: 2500
           }
     end
   end
