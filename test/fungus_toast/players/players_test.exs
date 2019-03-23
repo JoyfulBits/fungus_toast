d4351dc58f03b4ee6ef721c5670ec02f115cd356
jakejgordon <jakejgordon@gmail.com>
2019-03-22 23:13:51 -0400

    Created (failing) test for players. More to come.

--- test/fungus_toast/players/players_test.exs
+++ test/fungus_toast/players/players_test.exs
@@ -1,5 +1,5 @@
 defmodule FungusToast.PlayersTest do
-    use ExUnit.Case, async: false
+    use FungusToast.DataCase
     alias FungusToast.Games.Grid
     alias FungusToast.Games.Player
     alias FungusToast.Players
@@ -9,11 +9,12 @@ defmodule FungusToast.PlayersTest do
     describe "create_player_for_user/2" do
         test "that it creates a player with default skills" do
             game = Fixtures.Game.create!
-            player = Players.create_player_for_user("testUser", game)
+            user = Fixtures.Accounts.User.create!
+            {:ok, player} = Players.create_player_for_user(game, user.user_name)
 
-            assert length(player.skills) > 0
             assert player.id != nil
+            #TODO this passes when I run everything through IEX, but I have no idea why I can't
+            assert length(player.skills) > 0
         end
     end
 end
-  
