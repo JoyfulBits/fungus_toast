d4fa93f30db96de45595ef82a5f04a21b73605cb
jakejgordon <jakejgordon@gmail.com>
2019-04-12 11:32:01 -0400

    added super toxic AI and better output

--- test/fungus_toast/games/full_ai_game_test.exs
+++ test/fungus_toast/games/full_ai_game_test.exs
@@ -11,34 +11,36 @@ defmodule FungusToast.Games.FullAiGameTest do
     @tag :skip
     test "that two AI players can finish a game" do
 
-      Repo.transaction(fn ->
-        game_changeset = %Game{} |> Game.changeset(%{number_of_ai_players: 4, number_of_human_players: 0})
-        {:ok, game} = Repo.insert(game_changeset)
+      game_changeset = %Game{} |> Game.changeset(%{number_of_ai_players: 5, number_of_human_players: 0})
+      {:ok, game} = Repo.insert(game_changeset)
 
-        Players.create_basic_player(game.id, false, "Random AI", nil, AiStrategies.ai_type_random)
-        |> Player.changeset(%{})
-        |> Repo.insert()
+      Players.create_basic_player(game.id, false, "Random AI", nil, AiStrategies.ai_type_random)
+      |> Player.changeset(%{})
+      |> Repo.insert()
 
-        Players.create_basic_player(game.id, false, "Growth AI", nil, AiStrategies.ai_type_growth)
-        |> Player.changeset(%{})
-        |> Repo.insert()
+      Players.create_basic_player(game.id, false, "Growth AI", nil, AiStrategies.ai_type_growth)
+      |> Player.changeset(%{})
+      |> Repo.insert()
 
-        Players.create_basic_player(game.id, false, "Toxic AI", nil, AiStrategies.ai_type_toxic)
-        |> Player.changeset(%{})
-        |> Repo.insert()
+      Players.create_basic_player(game.id, false, "Toxic AI", nil, AiStrategies.ai_type_toxic)
+      |> Player.changeset(%{})
+      |> Repo.insert()
 
-        Players.create_basic_player(game.id, false, "Long Term AI", nil, AiStrategies.ai_type_long_term)
-        |> Player.changeset(%{})
-        |> Repo.insert()
+      Players.create_basic_player(game.id, false, "Long Term AI", nil, AiStrategies.ai_type_long_term)
+      |> Player.changeset(%{})
+      |> Repo.insert()
 
-        game = Repo.get(Game, game.id) |> Repo.preload(:players)
+      Players.create_basic_player(game.id, false, "Super Toxic AI", nil, AiStrategies.ai_type_experimental_1)
+      |> Player.changeset(%{})
+      |> Repo.insert()
 
-        Games.start_game(game)
+      game = Repo.get(Game, game.id) |> Repo.preload(:players)
 
-        game = Repo.get(Game, game.id) |> Repo.preload(:players)
+      Games.start_game(game)
 
-        play_game(game)
-      end)
+      game = Repo.get(Game, game.id) |> Repo.preload(:players)
+
+      play_game(game)
     end
 
     def play_game(game) do
@@ -49,8 +51,19 @@ defmodule FungusToast.Games.FullAiGameTest do
       if(game.status == Status.status_finished) do
         IO.inspect "***************"
         IO.inspect "GAME OVER"
-        ranked_map = Enum.sort(game.players, &(&1.live_cells >= &2.live_cells))
-        |> Enum.map(fn player -> {player.ai_type, player.live_cells} end)
+        Enum.sort(game.players, &(&1.live_cells >= &2.live_cells))
+        |> Enum.map(fn player -> %{
+          ai_type: player.ai_type,
+          live_cells: player.live_cells,
+          dead_cells: player.dead_cells,
+          regenerated_cells: player.regenerated_cells,
+          apoptosis_chance: player.apoptosis_chance,
+          regeneration_chance: player.regeneration_chance,
+          top_right_growth_chance: player.top_right_growth_chance,
+          mutation_chance: player.mutation_chance,
+          mycotoxin_fungicide_chance: player.mycotoxin_fungicide_chance
+        }
+            end)
         |> IO.inspect
       else
         if(round.number == 100) do
