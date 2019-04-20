1c6dc5beeaaae403ec4ad1d9d9a4937503ad2440
jakejgordon <jakejgordon@gmail.com>
2019-04-19 22:07:00 -0400

    fixed warnings

--- test/fungus_toast/games/helpers/rounds_test.exs
+++ test/fungus_toast/games/helpers/rounds_test.exs
@@ -1,12 +1,11 @@
 defmodule FungusToast.RoundsTest do
   use FungusToast.DataCase
   alias FungusToast.{Games, Rounds, Accounts}
-  alias FungusToast.Games.Game
 
   describe "get_latest_completed_round_for_game/1" do
     test "that the round doesn't have any growth cycles" do
       user_name = "testUser"
-      {:ok, user} = Accounts.create_user(%{user_name: user_name})
+      {:ok, _user} = Accounts.create_user(%{user_name: user_name})
       game = Games.create_game(user_name, %{number_of_human_players: 1})
 
       latest_round = Rounds.get_latest_completed_round_for_game(game.id)
