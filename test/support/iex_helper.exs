8293a191d0d702e0f87ef796f198280ac3509b4f
jakejgordon <jakejgordon@gmail.com>
2019-04-26 23:21:15 -0400

    minor conveniences

--- test/support/iex_helper.exs
+++ test/support/iex_helper.exs
@@ -1,7 +1,11 @@
 #This exists only as a convenience to copy/paste into iex
-alias FungusToast.{Repo, Accounts, Players, Games, Skills, PlayerSkills, Rounds}
+alias FungusToast.{Repo, Accounts, Players, Games, Skills, PlayerSkills, Rounds, AiStrategies}
 alias FungusToast.Accounts.User
 alias FungusToast.Games.{Game, Player, Grid, GridCell}
 
+#for creating a real user and game
+{:ok, user} = Accounts.create_user(%{user_name: "user 2019-04-26.01"})
+game = Games.create_game(user.user_name, %{number_of_human_players: 1, number_of_ai_players: 1})
 
+#for creating an empty game
 game = FungusToast.Games.Game.changeset(%FungusToast.Games.Game{}, %{number_of_human_players: 1, number_of_ai_players: 1}) |> Repo.insert!
