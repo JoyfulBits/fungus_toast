e17e123132b00c0c0ff76f93b30244bde1293c18
jakejgordon <jakejgordon@gmail.com>
2019-05-19 21:03:41 -0400

    removed commented out code

--- lib/fungus_toast_web/controllers/joined_game_controller.ex
+++ lib/fungus_toast_web/controllers/joined_game_controller.ex
@@ -7,7 +7,6 @@ defmodule FungusToastWeb.JoinedGameController do
 
   def create(conn, %{"game_id" => game_id, "user_name" => user_name}) do
     #TODO should the user id be passed in or the user name?
-    #TODO return a better error code when the user can't be found
     join_result = Games.join_game(game_id, user_name)
 
     case join_result do
@@ -28,44 +27,5 @@ defmodule FungusToastWeb.JoinedGameController do
         |> put_status(:internal_server_error )
         |> render("fail.json", result: nil)
     end
-
-    #   {:ok, next_round_available} ->
-    #     conn
-    #     |> put_status(:ok)
-    #     |> render("show.json", result: next_round_available)
-
-    #   _ ->
-    #     conn
-    #     |> put_status(:internal_server_error )
-    #     |> render("fail.json", result: nil)
-    # end
-
-    # case join_result do
-    #   {:error, error_reason} ->
-    #     case error_reason do
-    #       :no_open_slots ->
-    #         conn
-    #         |> put_status(:conflict)
-    #         |> render("game_full.json", %{})
-    #       :user_already_joined ->
-    #         conn
-    #         |> put_status(:conflict)
-    #         |> render("game_full.json", %{})
-    #       _ ->
-    #         conn
-    #         |> put_status(:internal_server_error )
-    #         |> render("fail.json", result: nil)
-    #     end
-
-    #   {:ok, next_round_available} ->
-    #     conn
-    #     |> put_status(:ok)
-    #     |> render("show.json", result: next_round_available)
-
-    #   _ ->
-    #     conn
-    #     |> put_status(:internal_server_error )
-    #     |> render("fail.json", result: nil)
-    # end
   end
 end
