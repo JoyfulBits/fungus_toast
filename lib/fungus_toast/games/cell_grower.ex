211f4aa24d58e559b3925137a7a01f5509f4b083
jakejgordon <jakejgordon@gmail.com>
2019-05-27 21:32:51 -0400

    fixed bug where starvation wasn't happening

--- lib/fungus_toast/games/cell_grower.ex
+++ lib/fungus_toast/games/cell_grower.ex
@@ -258,7 +258,7 @@ defmodule FungusToast.Games.CellGrower do
 
   """
   def dies_from_starvation(surrounding_cells, starvation_chance \\ @starvation_death_chance) do
-    Enum.all?(surrounding_cells, fn {_, v} -> v.live and Random.random_chance_hit(starvation_chance) end)
+    Enum.all?(surrounding_cells, fn {_, v} -> v.live end) and Random.random_chance_hit(starvation_chance)
   end
 
   @doc ~S"""
